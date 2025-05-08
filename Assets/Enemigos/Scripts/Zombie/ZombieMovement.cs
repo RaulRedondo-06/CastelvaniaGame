11eae9153c50b3422674249bdfa51e84f0104c21
INFOX-EQUIPXX\Argo <gvalls@ifp-alguero.org>
2025-05-08 20:49:32 +0200

    UI definitva + los enemy hacen daño :) viva el betis

--- Assets/Enemigos/Scripts/Zombie/ZombieMovement.cs
+++ Assets/Enemigos/Scripts/Zombie/ZombieMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,10 @@ using UnityEngine;
 public class ZombieMovement : MonoBehaviour
 {
     public float speed = 2f;
+    public float health;
+    public float maxHealth;
 
+    public float scoreObtained;
     private int direction = 1;
     private Vector3 startScale;
 
@@ -13,21 +17,40 @@ public class ZombieMovement : MonoBehaviour
     {
         startScale = transform.localScale;
         Flip(); // <- Voltea el sprite al comenzar según la dirección
+
+        maxHealth = health; // <-- vida del zombi
     }
 
     void Update()
     {
         transform.position += Vector3.right * speed * direction * Time.deltaTime;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+
+
+        //Debug.Log("Ta dentro");
         if (other.CompareTag("Turn"))
         {
+            //Debug.Log("Ta dentro i rebota");
             direction *= -1;
             Flip();
+        }//funciona :)
+
+        if (other.CompareTag("BULLET") || other.CompareTag("Latigo"))
+        {
+            Banck_acount.instance.Score(scoreObtained);
+            health -= 1;
         }
+
     }
+
 
     void Flip()
     {
