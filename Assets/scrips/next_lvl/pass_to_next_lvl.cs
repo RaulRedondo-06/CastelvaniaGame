1e0799e632c81c9aca38fa224bc314d8ce0a5f3b
Muhammad Anas <anasabid607@gmail.com>
2025-05-16 20:45:18 +0200

    musica

--- Assets/scrips/next_lvl/pass_to_next_lvl.cs
+++ Assets/scrips/next_lvl/pass_to_next_lvl.cs
@@ -7,7 +7,7 @@ public class pass_to_next_lvl : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) 
+        if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
